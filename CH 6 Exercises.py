9699842477da13d8447c16c35f953fad391b73fe
Helck19 <dafel1998@gmail.com>
2025-11-25 23:36:44 -0500

    Implement string reversal and letter counting function
    Added a while loop to reverse a string and print each character. Encapsulated letter counting in a function.

---
+++ CH 6 Exercises.py
@@ -0,0 +1,48 @@
+# Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.
+index=0
+
+while True:
+    try:
+       word=str(input("Please input the desired word: "))
+    except:
+        print("Please add a string")
+        continue
+    while index<len(word):
+        reverse=word[(-1)*(index+1)]
+        print(reverse)
+        index=index+1
+    control=input("Do you wish to do it again with another word? type yes or no:  ")
+    control=control.lower()
+    if control=="yes":
+        index=0
+        continue
+        
+    elif control=="no":
+        break
+#%%String slicing
+s="Monty python"
+print(s[0:5])
+print(s[6:])
+print(s[:len(s)])
+print(type(s[:]))
+print(s[:])
+
+#Strings are inmutable
+
+greeting= "Hello, world"
+new_greeting=greeting[:5]+" python and"+greeting[6:]
+print(new_greeting)
+
+#Excercise 3: Encapsulate this code in a function named, count, and generalize it so that it accepts the string and the letter as arguments.
+
+def count(word, letter_to_find):
+    count=0
+    for letter in word:
+        if letter==letter_to_find:
+            count=count+1
+    print("Amount of letter", letter_to_find, "found are: ", count)
+        
+
+count("banana", "a")
+
+  
