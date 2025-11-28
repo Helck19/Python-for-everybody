d00ee98334b7c5983291867de036a18b4158d1c6
Helck19 <dafel1998@gmail.com>
2025-11-27 22:15:27 -0500

    Implement Exercise 5 for string manipulation
    Added Exercise 5 to extract and convert a substring to float.

--- CH 6 Exercises.py
+++ CH 6 Exercises.py
@@ -52,6 +52,15 @@ for i in set(word):
     count=word.count(i)
     print("The number of times the letter: ", i, "appears in the word", word,"is: ", count)
 
+#%%Exercise 5: Take the following Python code that stores a string:
+string= 'X-DSPAM-Confidence:0.8475'
+#Use find and string slicing to extract the portion of the string after
+#the colon character and then use the float funciont to convert the extracted
+#string into a floating point number.
+index=string.find(":")
+number=float(string[index+1:])
+print(number)
+
 
 
   
